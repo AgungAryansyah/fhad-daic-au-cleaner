6150e157703bec67a075d64668696d38f5344fcd
AgungAryansyah <agungaryansyah2006@gmail.com>
2026-05-12 19:10:13 +0700

    fix: update config to match actual dataset column names

--- src/fhad_daic_au_cleaner/config.py
+++ src/fhad_daic_au_cleaner/config.py
@@ -10,14 +10,20 @@ EXCLUDED_SESSIONS = {342, 394, 398, 460, 373, 444, 451, 458, 480, 402}
 
 CONFIDENCE_THRESHOLD = 0.5
 
+LABEL_PARTICIPANT_COL = "Participant_ID"
+LABEL_PHQ_SCORE_COL = "PHQ_Score"
+LABEL_PHQ_BINARY_COL = "PHQ_Binary"
+
 AU_REGRESSION_COLS = [
-    "AU01_r", "AU02_r", "AU04_r", "AU05_r", "AU06_r",
-    "AU09_r", "AU10_r", "AU12_r", "AU14_r", "AU15_r",
-    "AU17_r", "AU20_r", "AU25_r", "AU26_r",
+    "AU01_r", "AU02_r", "AU04_r", "AU05_r", "AU06_r", "AU07_r",
+    "AU09_r", "AU10_r", "AU12_r", "AU14_r", "AU15_r", "AU17_r",
+    "AU20_r", "AU23_r", "AU25_r", "AU26_r", "AU45_r",
 ]
 
 AU_BINARY_COLS = [
-    "AU04_c", "AU12_c", "AU15_c", "AU23_c", "AU28_c", "AU45_c",
+    "AU01_c", "AU02_c", "AU04_c", "AU05_c", "AU06_c", "AU07_c",
+    "AU09_c", "AU10_c", "AU12_c", "AU14_c", "AU15_c", "AU17_c",
+    "AU20_c", "AU23_c", "AU25_c", "AU26_c", "AU28_c", "AU45_c",
 ]
 
 POSE_COLS = [
@@ -27,4 +33,4 @@ POSE_COLS = [
 
 FEATURE_COLS = AU_REGRESSION_COLS + AU_BINARY_COLS + POSE_COLS
 
-METADATA_COLS = ["participant_id", "phq8_score", "phq8_binary"]
+METADATA_COLS = ["participant_id", "phq_score", "phq_binary"]
